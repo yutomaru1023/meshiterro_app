f1567205f8cc4dc7ffc57a542b685580da32e500
EC2 Default User <ec2-user@ip-172-31-33-190.ap-northeast-1.compute.internal>
2022-09-28 03:43:30 +0000

    meshiterro[chapter20]

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about",as:"about"
    
   resources :post_images, only: [:new,:create,:index,:show,:destroy] do
-    resources :post_comments, only: [:create]
+    resource :favorites, only:[:create, :destroy]
+    
+    resources :post_comments, only: [:create,:destroy]
   end
   
   resources :users, only:[:show,:edit,:update]
